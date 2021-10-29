ce108c33503f64bec8dc6af95e0261d16c830ba0
Tanishjaingit10 <Tanish.jain.961@gmail.com>
2021-10-29 17:44:44 +0530

    theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,22 +12,22 @@ export const Navbar = () => {
     }
     
     return (
-        <div className="flex absolute justify-between h-20 z-10 w-full bg-gray-50 shadow-lg" style ={{position:'fixed', top:'0px'}}>
-            <Link to="/" className="flex items-center justify-center w-2/12 text-xl text-gray-700 font-bold no-underline"><div>Myntra</div></Link>
+        <div className="flex absolute justify-between h-20 z-10 w-full bg-gray-50 shadow-md" style ={{position:'fixed', top:'0px'}}>
+            <Link to="/" className="flex items-center justify-center w-2/12 text-xl text-gray-700 font-bold no-underline text-pink-500 hover:underline hover:text-pink-500"><div>Myntra</div></Link>
             <div className="flex ">
                 {
                     loggedIn ?
                     <>
-                        <Link to="/games" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Games</div></Link>
-                        <Link to="/profile" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Profile</div></Link>
-                        <Link to="/cart" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Cart</div></Link>
-                        <Link to="/contact" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Contact</div></Link>
-                        {/* <button onClick={logout} className="flex items-center justify-center text-lg m-8 text-gray-700"><div>Logout</div></button> */}
+                        <Link to="/games" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline hover:text-pink-500"><div>Games</div></Link>
+                        <Link to="/profile" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline hover:text-pink-500"><div>Profile</div></Link>
+                        <Link to="/cart" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline hover:text-pink-500"><div>Cart</div></Link>
+                        <Link to="/contact" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline hover:text-pink-500"><div>Contact</div></Link>
+                        <button onClick={logout} className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline hover:text-pink-500"><div>Logout</div></button>
                     </>
                     :
                     <>
-                        <Link to="/signin" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Login</div></Link>
-                        <Link to="/signup" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Signup</div></Link>
+                        <Link to="/signin" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline hover:text-pink-500"><div>Login</div></Link>
+                        <Link to="/signup" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline hover:text-pink-500"><div>Signup</div></Link>
                     </>
                 }
             </div>
--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -32,21 +32,21 @@ function Login() {
         console.log(error)
     }
     return (
-        <div className="h-screen pt-20 flex items-center justify-center w-full">
-            <div className="p-10 pt-8 w-11/12 md:w-3/5 sm:w-5/6 lg:w-1/2 rounded-2xl bg-gray-900 shadow-2xl flex items-center flex-col">
-                <h1 className="mb-2 text-3xl p-3"> Signin </h1>
+        <div className="h-screen pt-20 flex items-center bg-gray-100 justify-center w-full">
+            <div className="p-10 pt-8 w-11/12 md:w-3/5 sm:w-5/6 lg:w-1/2 rounded-2xl bg-white shadow-md flex items-center flex-col">
+                <h1 className="mb-2 text-3xl p-3 text-pink-500"> Signin </h1>
                 <div className="w-11/12 sm:w-5/6 md:3/4 lg:w-2/3 xl:w-1/2 flex flex-col items-center">
-                    <form className="w-full flex flex-col " onSubmit={(e)=>loginHandler(e)}>
+                    <form className="w-full flex flex-col text-gray-700" onSubmit={(e)=>loginHandler(e)}>
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={e=>setEmail(e.target.value)} className="mb-4 p-1 w-full rounded text-black px-2" required/>
+                        <input type="email" id="email" onChange={e=>setEmail(e.target.value)} className="shadow-sm mb-4 p-1 w-full rounded px-2 border" required/>
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={e=>setPassword(e.target.value)} className="mb-4 p-1 w-full rounded text-black px-2" required/>
-                        <button type="submit" className="w-full shadow rounded my-3 p-2 bg-blue-800 font-bold">Login</button>
+                        <input type="password" id="password" onChange={e=>setPassword(e.target.value)} className="shadow-sm mb-4 p-1 w-full rounded px-2 border" required/>
+                        <button type="submit" className="w-full shadow-md rounded my-3 p-2 bg-blue-500 font-bold text-white">Login</button>
                     </form>
                     <GoogleLogin
                     clientId="920754705442-j7429tvbnqpmbtehalk80rkhlgt2975r.apps.googleusercontent.com"
                     render={e => (
-                        <button onClick={e.onClick} disabled={e.disabled} className="w-full shadow rounded my-5 p-2 bg-red-800 font-bold">
+                        <button onClick={e.onClick} disabled={e.disabled} className="w-full text-white shadow-md rounded my-4 p-2 bg-red-500 font-bold">
                             G
                         </button>
                     )}
