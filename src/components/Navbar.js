5316d797caebfcb5309473927590b94df8cd52d5
Ved <vedsharda@gmail.com>
2021-10-29 16:06:20 +0530

    home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,19 +12,22 @@ export const Navbar = () => {
     }
     
     return (
-        <div className="flex absolute justify-between h-20 w-full bg-black shadow-lg">
-            <Link to="/" className="flex items-center justify-center w-2/12 text-xl text-white"><div>Myntra Dark</div></Link>
-            <div className="flex">
+        <div className="flex absolute justify-between h-20 z-10 w-full bg-gray-50 shadow-lg" style ={{position:'fixed', top:'0px'}}>
+            <Link to="/" className="flex items-center justify-center w-2/12 text-xl text-gray-700 font-bold no-underline"><div>Myntra</div></Link>
+            <div className="flex ">
                 {
                     loggedIn ?
                     <>
-                        <Link to="/profile" className="flex items-center justify-center text-lg m-8 text-white"><div>Profile</div></Link>
-                        <button onClick={logout} className="flex items-center justify-center text-lg m-8 text-white"><div>Logout</div></button>
+                        <Link to="/games" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Games</div></Link>
+                        <Link to="/profile" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Profile</div></Link>
+                        <Link to="/cart" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Cart</div></Link>
+                        <Link to="/contact" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Contact</div></Link>
+                        {/* <button onClick={logout} className="flex items-center justify-center text-lg m-8 text-gray-700"><div>Logout</div></button> */}
                     </>
                     :
                     <>
-                        <Link to="/signin" className="flex items-center justify-center text-lg m-8 text-white"><div>Login</div></Link>
-                        <Link to="/signup" className="flex items-center justify-center text-lg m-8 text-white"><div>Signup</div></Link>
+                        <Link to="/signin" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Login</div></Link>
+                        <Link to="/signup" className="flex items-center justify-center text-lg m-8 text-gray-700 font-bold no-underline"><div>Signup</div></Link>
                     </>
                 }
             </div>
---
+++ src/components/screens/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cart() {
+    return (
+        <div className="h-screen w-screen flex items-center justify-center">
+            Cart
+        </div>
+    )
+}
+
+export default Cart
