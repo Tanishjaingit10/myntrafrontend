84233a8318138d49fe75ecdb8d409ee2b4cf7b89
Siddhant Jaiswal <siddhantjaiswal.mst18@iitbhu.ac.in>
2021-10-31 21:32:27 +0530

    contact complete with footer

--- src/components/screens/Contact.js
+++ src/components/screens/Contact.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaUser, FaShoppingCart, FaPhone, FaGamepad} from 'react-icons/fa'
 
 function Contact() {
     return (
+    <div>
         <div className="container">
             <div className="row" style={{ marginTop:'100px'}}>
                 <div className="col-12">
@@ -23,7 +25,7 @@ function Contact() {
                 </div>
                 <div className="col-12 col-sm-4 offset-sm-1">
                     <p>CIN: U72300KA2007PTC041799</p>
-                    <p>Telephone: +91-80-61561999</p>
+                    <p><b>Telephone</b>: +91-80-61561999</p>
                 </div>
             </div>
             <div className="row row-content">
@@ -75,9 +77,41 @@ function Contact() {
                         </div>
                     </form>
                 </div>
-            </div>
+            </div>            
         </div>
-        )
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-6  links">
+                        <h5>Useful Links</h5>
+                        <br/>
+                        <ul class="list-unstyled d-sm-flex justify-content-center">
+                            <li><a class="btn btn-lg" href=""><FaGamepad/></a></li>
+                            <li><a class="btn btn-lg" href=""><FaUser /></a></li>
+                            <li><a class="btn btn-lg" href=""><FaShoppingCart /></a></li>
+                            <li><a class="btn btn-lg" href=""><FaPhone /></a></li>                        
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-6 social">
+                        <h5>Keep In Touch</h5>
+                        <br/>
+                        <ul class="list-unstyled d-sm-flex justify-content-center">
+                            <li><a class="btn btn-facebook btn-lg" href="https://www.facebook.com/myntra"><FaFacebook /></a></li>
+                            <li><a class="btn btn-twitter btn-lg" href="https://twitter.com/myntra"><FaTwitter /></a></li>
+                            <li><a class="btn btn-google btn-lg" href="https://www.youtube.com/c/myntra"><FaYoutube /></a></li>
+                            <li><a class="btn btn-instagram btn-lg" href="https://www.instagram.com/myntra/"><FaInstagram /></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-auto">
+                        <p>© 2021 www.myntra.com. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    )
 }
 
 export default Contact
