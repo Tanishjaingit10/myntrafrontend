924d487546222dd63cb75bc32d2d7108a674e5db
Tanishjaingit10 <Tanish.jain.961@gmail.com>
2021-11-06 15:43:40 +0530

    merge

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BACKEND } from '../../Keys';
 import { Carousel, Card, Button, Row, Col, Modal } from "react-bootstrap";
 import Footer from "../Footer"
@@ -28,9 +28,23 @@ function MyVerticallyCenteredModal(props) {
 
 function Home() {
     // wake call to backend
-    axios.get(`${BACKEND}`)
-
+    const [top3, setTop3] = useState([])
+    useEffect(() => {
+        axios.get(`${BACKEND}`)
+        console.log(`${BACKEND}/stars`)
+        axios.get(`${BACKEND}/stars`)
+        .then(res => {
+            setTop3(res.data)
+        })
+        .catch(err=>console.log(err))
+    }, [])
+    
+    const img1 = "https://assets.myntassets.com/w_980,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2019/3/31/e27302d6-a2c4-4fd3-a2b1-c3bd92a8b2401554050196883-ESPRIT_Desktop_Banner.jpg";
+    const img2 = "https://assets.myntassets.com/w_980,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2019/3/30/e5a955ac-e224-4b83-aaac-e86674ddb7471553959219090-Zaveri_Desktop_Banner.jpg";
+    const img3 = "https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
     const [modalShow, setModalShow] = React.useState(false);
+
+    // const [modalShow, setModalShow] = React.useState(false);
     return (
         <div>
             <div className="container-fluid">
@@ -115,47 +129,54 @@ function Home() {
                         }
                     </Row>
                 </div>
-                <h2 style={{ marginLeft: '10px' }}>Stars of the Week</h2>
-                <div className="container-fluid" style={{ marginTop: '10px', marginBottom: '40px' }}>
-                    <Row xs={1} md={2} className="g-4" style={{ marginTop: '20px' }}>
-                        <Col style={{ margin: 'auto' }}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title className="text-lg text-center m-8 text-gray-700 font-bold">Winner</Card.Title>
-                                    <Card.Text className="text-lg text-center m-8 text-gray-700 font-bold">
-                                        Name & Coins
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row xs={1} md={2} className="g-4" style={{ marginTop: '5px' }}>
-                        <Col>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title className="text-lg text-center m-8 text-gray-700 font-bold">Second</Card.Title>
-                                    <Card.Text className="text-lg text-center m-8 text-gray-700 font-bold">
-                                        Name & Coins
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title className="text-lg text-center m-8 text-gray-700 font-bold">Third</Card.Title>
-                                    <Card.Text className="text-lg text-center m-8 text-gray-700 font-bold">
-                                        Name & Coins
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                </div>
+                {
+                    top3.length>2 &&
+                    <>
+                        <h2 style={{ marginLeft: '10px' }}>Star of the Week</h2>
+                        <div className="container-fluid" style={{ marginTop: '10px', marginBottom: '40px' }}>
+                            <Row xs={1} md={2} className="g-4" style={{ marginTop: '20px' }}>
+                                <Col style={{ margin: 'auto' }}>
+                                    <Card>
+                                        <Card.Body>
+                                            <Card.Title className="text-lg text-center m-8 text-gray-700 font-bold">Winner</Card.Title>
+                                            <Card.Text className="text-lg text-center m-8 text-gray-700 font-bold">
+                                                {top3[0].name}<br/>
+                                                {top3[0].coins}
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                            <Row xs={1} md={2} className="g-4" style={{ marginTop: '5px' }}>
+                                <Col>
+                                    <Card>
+                                        <Card.Body>
+                                            <Card.Title className="text-lg text-center m-8 text-gray-700 font-bold">Second</Card.Title>
+                                            <Card.Text className="text-lg text-center m-8 text-gray-700 font-bold">
+                                                {top3[1].name}<br/>
+                                                {top3[1].coins}
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col>
+                                    <Card>
+                                        <Card.Body>
+                                            <Card.Title className="text-lg text-center m-8 text-gray-700 font-bold">Third</Card.Title>
+                                            <Card.Text className="text-lg text-center m-8 text-gray-700 font-bold">
+                                                {top3[2].name}<br/>
+                                                {top3[2].coins}
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </div>
+                    </>
+                }
             </div>
-            <Footer />
         </div>
-    )
+    );
 }
 
 export default Home
