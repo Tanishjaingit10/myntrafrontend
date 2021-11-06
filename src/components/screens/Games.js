4a783d37a2d03fc16c8f659b94bff0eb986f640f
Ved <vedsharda@gmail.com>
2021-11-06 16:54:10 +0530

    game added

--- src/components/screens/Games.js
+++ src/components/screens/Games.js
@@ -1,15 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Footer from "../Footer"
+import "./games.css"
 
 function Games() {
+    const questions = [
+        {
+            questionText: 'What is the capital of France?',
+            answerOptions: [
+                { answerText: 'New York', isCorrect: false },
+                { answerText: 'London', isCorrect: false },
+                { answerText: 'Paris', isCorrect: true },
+                { answerText: 'Dublin', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'Who is CEO of Tesla?',
+            answerOptions: [
+                { answerText: 'Jeff Bezos', isCorrect: false },
+                { answerText: 'Elon Musk', isCorrect: true },
+                { answerText: 'Bill Gates', isCorrect: false },
+                { answerText: 'Tony Stark', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'The iPhone was created by which company?',
+            answerOptions: [
+                { answerText: 'Apple', isCorrect: true },
+                { answerText: 'Intel', isCorrect: false },
+                { answerText: 'Amazon', isCorrect: false },
+                { answerText: 'Microsoft', isCorrect: false },
+            ],
+        },
+        {
+            questionText: 'How many Harry Potter books are there?',
+            answerOptions: [
+                { answerText: '1', isCorrect: false },
+                { answerText: '4', isCorrect: false },
+                { answerText: '6', isCorrect: false },
+                { answerText: '7', isCorrect: true },
+            ],
+        },
+    ];
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
+
+    const handleAnswerOptionClick = (isCorrect) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
     return (
         <div>
-            <div className="h-screen w-screen flex items-center justify-center">
-                Games
+            <div style={{ marginTop: '120px' }}>
+                <h2 className="text-center mb-5">Play your daily quiz here to earn Myntra coins</h2>
+                <div className="app mb-5" style={{ margin: 'auto' }}>
+                    {showScore ? (
+                        <div className='score-section'>
+                            You scored {score} out of {questions.length} <br/>
+                            & earned {4*score} Myntra Coins
+                        </div>
+                    ) : (
+                        <>
+                            <div className='question-section'>
+                                <div className='question-count'>
+                                    <span>Question {currentQuestion + 1}</span>/{questions.length}
+                                </div>
+                                <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                            </div>
+                            <div className='answer-section'>
+                                {questions[currentQuestion].answerOptions.map((answerOption) => (
+                                    <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                                ))}
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
             <Footer />
         </div>
-    )
+    );
 }
-
 export default Games
