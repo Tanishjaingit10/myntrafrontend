3bba2bc692db6ed9e5c7a1f903b25f3226802981
Siddhant Jaiswal <siddhantjaiswal.mst18@iitbhu.ac.in>
2021-11-07 14:26:24 +0530

    learn to earn update 2

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -63,10 +63,10 @@ function Profile() {
                                 <Accordion.Header>Learn how to earn Myntra coins</Accordion.Header>
                                 <Accordion.Body>
                                     1) First time SignUp into Myntra - <b>100 coins</b><br/>
-                                    2) Daily Login using your account - <b>10 coins</b><br />
-                                    3) Submit your feedback regarding products, features, etc - <b>10 coins</b><br />
-                                    4) Playing daily quizzes on games page - <b>4 coins</b> per correct answer<br />
-                                    5) Ask your friends to use your referral code to sign up into Myntra - <b>100 coins</b><br/>
+                                    2) Ask your friends to use your referral code to sign up into Myntra - <b>100 coins</b><br/>
+                                    3) Daily Login using your account - <b>10 coins</b><br />
+                                    4) Submit your feedback regarding products, features, etc - <b>10 coins</b><br/>
+                                    5) Playing daily quizzes on games page - <b>4 coins</b> per correct answer<br/>                 
                                     6) Stars of the Week - <b>50 bonus coins</b><br/>
                                     7) Purchasing product - <b>10% bonus coins</b> of total price<br/>
                                 </Accordion.Body>
